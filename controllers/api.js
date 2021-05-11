8fb4047daf7f7979e1fc6ffa245dc9b6f75a028a
Blake DeGraw <bbdegraw@gmail.com>
2021-05-11 15:30:22 -0700

    pizzarias post is working with price from client

--- controllers/api.js
+++ controllers/api.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Prices = require('../models/Prices')
 const Types = require('../models/Types')
+const Pizzarias = require('../models/Pizzarias')
 
 require('dotenv').config()
 
@@ -13,6 +14,13 @@ router.get('/prices', (req, res) => {
 router.get('/types', (req, res) => {
     Types.find({})
         .then(typeData => res.send(typeData))
+})
+
+router.post('/pizzarias', (req, res) => {
+    console.log("receiving from back end")
+    console.log(req.body.thing)
+    Pizzarias.find({priceRange: req.body.thing})
+        .then(pizzariaData => res.send(pizzariaData))
 })
 
 module.exports = router
--- index.js
+++ index.js
@@ -2,10 +2,13 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser');
 
 app.use(cors())
+app.use(express.json())
 
 app.use(express.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // app.get('/', (req, res) => {
 //     res.send('sanity check')
