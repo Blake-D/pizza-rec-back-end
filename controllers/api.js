f658f50f70b42b2ada6ee83ff0ed6d99053fa353
Blake DeGraw <bbdegraw@gmail.com>
2021-05-11 12:16:01 -0700

    types route communicating with front end

--- controllers/api.js
+++ controllers/api.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const Prices = require('../models/Prices')
+const Types = require('../models/Types')
 
 require('dotenv').config()
 
 router.get('/prices', (req, res) => {
     Prices.find({})
         .then(priceData => res.send(priceData))
-        .then(console.log("sent"))
+})
+
+router.get('/types', (req, res) => {
+    Types.find({})
+        .then(typeData => res.send(typeData))
 })
 
 module.exports = router
